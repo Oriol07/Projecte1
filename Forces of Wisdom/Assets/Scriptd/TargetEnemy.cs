954caa80dc82313aaea1d1db2d06b70fd0913f15
Unknown <oriol799@gmail.com>
2018-04-20 11:57:20 +0200

    Target
    Esta per acabar, pero funciona

---
+++ Forces of Wisdom/Assets/Scriptd/TargetEnemy.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetEnemy : MonoBehaviour {
+
+    public List<Transform> objectius;
+    public Transform selectedTarget;
+    private Transform myTransform;
+    // Use this for initialization
+    void Start () {
+        objectius = new List<Transform>();
+        AfegirEnemics();
+        myTransform = transform;
+        
+	}
+	
+    public void AfegirEnemics()
+    {
+        GameObject[] go = GameObject.FindGameObjectsWithTag("Enemic");
+
+        selectedTarget = null;
+        foreach (GameObject enemic in go)
+            AfegirObjectiu(enemic.transform);
+    }
+
+    public void AfegirObjectiu(Transform enemic)
+    {
+        objectius.Add(enemic);
+    }
+
+    private void TargetEnemic()
+    {
+        if (selectedTarget == null)
+        {
+            TargetsPerDist();
+            selectedTarget = objectius[0];
+        }
+        else
+        {
+            int index = objectius.IndexOf(selectedTarget);
+            if (index < objectius.Count - 1)
+            {
+                index++;
+            }
+            else
+            {
+                index = 0;
+            }
+            selectedTarget = objectius[index];
+        }
+    }
+    void TargetsPerDist()
+    {
+        objectius.Sort(delegate(Transform t1, Transform t2) {
+                  return Vector3.Distance(t1.position, myTransform.position).CompareTo(Vector3.Distance(t2.position, myTransform.position));
+            });
+    }
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            TargetEnemic();
+        }
+		
+	}
+}
